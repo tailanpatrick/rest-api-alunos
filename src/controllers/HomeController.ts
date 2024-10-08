47d574100bacf3346d4ecd92b065c0bdcbaa84f4
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-10-08 15:46:58 -0300

    added table users to schema.prisma

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import { prismaClient } from '../db/PrismaClient'
 
 class HomeController {
 
-  index(req: Request, res: Response) {
+  async index(req: Request, res: Response) {
+    // exemplo de criação de um registro com o Prisma
+    const novoAluno = await prismaClient.student.create({
+      data: {
+        name: 'Maria',
+        surname: 'Miranda',
+        email: 'maria@gmail.com',
+        height: 1.65,
+        weight: 57,
+        age: 27
+      }
+    });
 
     res.status(200).json({
-      tudoCerto: true
+      novoAluno
     });
   }
 

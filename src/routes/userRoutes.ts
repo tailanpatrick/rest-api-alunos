7f93cadcc75c52da98939ba4602c42083a2b1f08
Tailan Patrick <tailanpatrick06@gmail.com>
2024-11-19 10:33:03 -0300

    added search route by id and email

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,7 +5,8 @@ const userRoutes: Router = Router();
 
 userRoutes.post('/', userController.create);
 userRoutes.get('/', userController.index);
-userRoutes.get('/:id', userController.show);
+userRoutes.get('/id/:id', userController.showById);
+userRoutes.get('/email/:email', userController.showByEmail);
 userRoutes.put('/:id', userController.update);
 userRoutes.delete('/:id', userController.delete);
 

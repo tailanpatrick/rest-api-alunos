654175afa3a352fda1fde30c1811353cadabf260
Tailan Patrick <tailanpatrick06@gmail.com>
2024-11-20 07:02:20 -0300

    Testing authentication middleware to verify JWT token

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const userRoutes: Router = Router();
 
 userRoutes.post('/', userController.create);
-userRoutes.get('/', userController.index);
+userRoutes.get('/', loginRequired, userController.index);
 userRoutes.get('/id/:id', userController.showById);
 userRoutes.get('/email/:email', userController.showByEmail);
 userRoutes.put('/:id', userController.update);

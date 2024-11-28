06d96d22db56d527884d8c181f58dc2a135c77d7
Tailan Patrick <tailanpatrick06@gmail.com>
2024-11-28 11:40:08 -0300

    configured the multer to handle the upload of photos

---
+++ src/config/multerConfig.ts
@@ -0,0 +1,27 @@
+import multer, { FileFilterCallback, MulterError } from 'multer';
+import { extname, resolve } from 'path';
+import { Request } from 'express';
+
+const random = () => Math.floor(Math.random() * 10000 * 10000)
+
+const imageFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
+    const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
+
+    if(allowedMimeTypes.includes(file.mimetype)){
+        cb(null, true);
+    } else {
+        cb(new MulterError('LIMIT_UNEXPECTED_FILE', 'O arquivo precisa ser: JPEG, JPG, PNG ou GIF'));
+    }
+}
+
+export default {
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, resolve(__dirname, '..', '..', 'uploads'))
+        },
+        filename: (req, file, cb) => {
+            cb(null, `${Date.now()}_${random()}${extname(file.originalname)}`)
+        }
+    }),
+    fileFilter: imageFilter
+}

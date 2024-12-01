4db968c824f51a01bda055557020166fbdbb588f
Tailan Patrick <tailanpatrick06@gmail.com>
2024-12-01 11:44:21 -0300

    closing and commenting routes for deploying the api

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,11 +5,11 @@ import loginRequired from '../middlewares/loginRequired';
 const userRoutes: Router = Router();
 
 //TODO: Remover essas rotas na versão final
-userRoutes.get('/', loginRequired, userController.index);
-userRoutes.get('/id/:userId', userController.showById);
-userRoutes.get('/email/:email', userController.showByEmail);
+// userRoutes.get('/', loginRequired, userController.index);
+// userRoutes.get('/id/:userId', loginRequired, userController.showById);
+// userRoutes.get('/email/:email', loginRequired, userController.showByEmail);
 
-userRoutes.post('/', userController.create);
+userRoutes.post('/', loginRequired, userController.create);
 userRoutes.put('/', loginRequired, userController.update);
 userRoutes.delete('/', loginRequired, userController.delete);
 

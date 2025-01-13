09c6c61cd32c14430eaacec124a83e915e9326c8
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-13 11:25:05 -0300

    configured cors

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,7 +5,7 @@ import loginRequired from '../middlewares/loginRequired';
 const userRoutes: Router = Router();
 
 //TODO: Remover essas rotas na versão final
-// userRoutes.get('/', loginRequired, userController.index);
+userRoutes.get('/', userController.index);
 // userRoutes.get('/id/:userId', loginRequired, userController.showById);
 // userRoutes.get('/email/:email', loginRequired, userController.showByEmail);
 

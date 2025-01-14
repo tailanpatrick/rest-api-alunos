8fe089fc69d397269abf140e82d46772f43c964e
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-14 06:23:07 -0300

    open route to create a user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,11 +5,11 @@ import loginRequired from '../middlewares/loginRequired';
 const userRoutes: Router = Router();
 
 //TODO: Remover essas rotas na versão final
-userRoutes.get('/', userController.index);
+//userRoutes.get('/', userController.index);
 // userRoutes.get('/id/:userId', loginRequired, userController.showById);
 // userRoutes.get('/email/:email', loginRequired, userController.showByEmail);
 
-userRoutes.post('/', loginRequired, userController.create);
+userRoutes.post('/', userController.create);
 userRoutes.put('/', loginRequired, userController.update);
 userRoutes.delete('/', loginRequired, userController.delete);
 

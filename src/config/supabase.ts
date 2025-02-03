f1eae857f9815f4061298e17525eb35857a41015
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-03 08:43:08 -0300

    fix supabase

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -2,6 +2,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ynmpgcgwwrvvjrlbkvwi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlubXBnY2d3d3J2dmpybGJrdndpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODA2OTIzNCwiZXhwIjoyMDUzNjQ1MjM0fQ.xHX-LTdgnsPy3uZcp77TRdhdNHwE8_UU9jL46V4llpw';
-
+const supabaseKey = process.env.SUPABASE_KEY || '';
 export const supabase = createClient(supabaseUrl, supabaseKey);

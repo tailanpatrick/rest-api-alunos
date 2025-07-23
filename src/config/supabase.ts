259664dd99ba845a29bb3b831004d742b7028cd5
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-07-23 13:45:41 -0300

    alterado para Url ser buscada no env

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,6 +1,6 @@
 // src/config/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ynmpgcgwwrvvjrlbkvwi.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || '';
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_KEY as string;
 export const supabase = createClient(supabaseUrl, supabaseKey);
